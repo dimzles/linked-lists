060b3b1276c4b41e4e78ee84b5949c071fe5d12d
dimzles <dimzlestv@gmail.com>
2023-05-11 15:08:35 +0100

    Add append function to linked list class

--- linkedLists.js
+++ linkedLists.js
@@ -9,4 +9,21 @@ class LinkedList {
   constructor() {
     this.listHead = null;
   }
+
+  append(value) {
+    //create newNode containing value
+    const newNode = new Node(value);
+
+    //if !listHead, listHead = newNode
+    if (this.listHead === null) this.listHead = newNode;
+    //otherwise, continue down the nextNodes until hit null
+    else {
+      let listTail = this.listHead;
+      while (listTail.nextNode !== null) {
+        listTail = listTail.nextNode;
+      }
+      //when tail = null, tail.nextNode = newNode
+      listTail.nextNode = newNode;
+    }
+  }
 }
